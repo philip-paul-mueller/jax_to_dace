b31cc770304305b7f30f495439a09304dacbbdcf
Philip Mueller <philip.mueller@cscs.ch>
2023-11-16 13:12:50 +0100

    Added a broadcast implementation that builds on the reshape implementation of the python frontend.
    There is no test for that yet.

--- JaxprToSDFG/translators/__init__.py
+++ JaxprToSDFG/translators/__init__.py
@@ -4,10 +4,12 @@
 from JaxprToSDFG.JaxIntrinsicTranslatorInterface    import JaxIntrinsicTranslatorInterface
 from .simpleTranslator                              import SimpleTransformator
 from .slicingTranslator                             import SlicingTransformator
+from .broadcastinDimTranslator                      import BroadcastInDimTransformator
 
 ALL_TRAFOS = [
     SimpleTransformator,
     SlicingTransformator,
+    BroadcastInDimTransformator,
 ]
 
 
