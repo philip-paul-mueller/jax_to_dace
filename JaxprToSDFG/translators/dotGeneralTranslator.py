687bc922cd33f51206f4cc15228843a615b8a41b
Christos Kotsalos <kotsaloscv@gmail.com>
2023-11-17 16:38:44 +0100

    Partial implementation of dot_general

--- JaxprToSDFG/translators/dotGeneralTranslator.py
+++ JaxprToSDFG/translators/dotGeneralTranslator.py
@@ -63,8 +63,19 @@ class DotGeneralTranslator(JaxIntrinsicTranslatorInterface):
         output_is_scalar = (len(eqn.outvars[0].aval.shape) == 0)
         lhs_shape = eqn.invars[0].aval.shape
         rhs_shape = eqn.invars[1].aval.shape
+        
+        if len(lhs_shape) > 2 or len(rhs_shape) > 2:
+            # TODO: include more cases
+            raise NotImplementedError('Partially implemented dot_general') 
 
-        if len(lhs_shape) == 1 and len(rhs_shape) == 1:
+        contraction_dims = eqn.params['dimension_numbers'][0]
+        batch_dims = eqn.params['dimension_numbers'][1]
+        
+        if batch_dims != ((), ()):
+            # TODO: include more cases
+            raise NotImplementedError('Partially implemented dot_general')
+
+        if (len(lhs_shape) == 1 and len(rhs_shape) == 1) and contraction_dims == ((0,), (0,)):
             from dace.frontend.python.replacements import dot
             dot(None,
                 translator.m_sdfg,
@@ -73,7 +84,7 @@ class DotGeneralTranslator(JaxIntrinsicTranslatorInterface):
                 inVarNames[1],
                 outVarNames[0],
                 )
-        elif len(lhs_shape) == 2 or len(rhs_shape) == 2:
+        elif (len(lhs_shape) == 2 or len(rhs_shape) == 2) and (contraction_dims == ((1,), (0,)) or contraction_dims == ((0,), (0,))):
             from dace.libraries.blas.nodes import MatMul
             libnode = MatMul('_MatMult_')
             eqnState.add_node(libnode)
@@ -86,7 +97,8 @@ class DotGeneralTranslator(JaxIntrinsicTranslatorInterface):
             eqnState.add_edge(gY, None, libnode, '_b', dace.memlet.Memlet.from_array(gY.data, translator.m_sdfg.arrays[inVarNames[1]]))
             eqnState.add_edge(libnode, '_c', gZ, None, dace.memlet.Memlet.from_array(gZ.data, translator.m_sdfg.arrays[outVarNames[0]]))
         else:
-            raise NotImplementedError
+            # TODO: include more cases (see tensordot)
+            raise NotImplementedError('Partially implemented dot_general')
 
         return eqnState
     # end def: translateEqn
