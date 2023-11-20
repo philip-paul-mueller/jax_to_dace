446635e5d3ef48ed6f332ccb96aed446625263e3
Philip Mueller <philip.mueller@cscs.ch>
2023-11-20 09:47:28 +0100

    Updated the `BroadcastInDimTransformator` class.
    It is now able to handle literals (at least scalar ones) and scalar variables as arguments.

--- JaxprToSDFG/translators/broadcastinDimTranslator.py
+++ JaxprToSDFG/translators/broadcastinDimTranslator.py
@@ -3,9 +3,11 @@
 from JaxprToSDFG.JaxIntrinsicTranslatorInterface import JaxIntrinsicTranslatorInterface
 
 from jax._src.core import JaxprEqn
+import numpy as np
 import dace
 from dace import subsets
 from typing import Union
+from math import prod
 
 
 class BroadcastInDimTransformator(JaxIntrinsicTranslatorInterface):
@@ -68,21 +70,40 @@ class BroadcastInDimTransformator(JaxIntrinsicTranslatorInterface):
             Jax only allows that the slicing parameters have static values.
             While the implementation could potentially handle a step size not equal than 1, Jax seems to implement that a bit different.
         """
-        inShape  = eqn.invars[0].aval.shape
-        outShape = eqn.outvars[0].aval.shape
-        shape    = eqn.params['shape'               ]
-        bDims    = eqn.params['broadcast_dimensions']
+        outShape     = eqn.outvars[0].aval.shape
+        shape        = eqn.params['shape'               ]
+        bDims        = eqn.params['broadcast_dimensions']
+        inpIsLiteral = False
+        inpIsScalar  = False
+
+        if(inVarNames[0] is None):
+            assert bDims ==  ()
+            inpIsLiteral = True
+            inShape      = ()
+
+        elif((eqn.invars[0].aval.shape == ()) and (isinstance(translator.getArray(inVarNames[0]), dace.data.Scalar))):
+            # We have a scalar and we will handle it the same as we would do a literal.
+            inpIsScalar = True
+            inShape     = ()
+
+        else:
+            inShape  = eqn.invars[0].aval.shape
+            assert len(bDims) > 0, f"Expected to find an array, but found a scalarish thing."
+        #
 
         if(len(eqn.invars) != 1):
             raise ValueError(f"`broadcast_in_dim` only supports one input argument.")
-        assert inVarNames[0] is not None
         if(len(eqn.outvars) != 1):
             raise ValueError(f"Expected only one return value of equation '{str(eqn)}' but it had {len(eqn.outvars)}")
         if(eqn.outvars[0].aval.shape != shape):
             raise ValueError(f"Parameters specified a shape of `{shape}` but the output variable ad a shape of `{eqn.outvars[0].aval.shape}`.")
         if(outShape != shape):
             raise ValueError(f"shape of the output was `{outShape}`, but the specified shape was `{shape}`.")
-        if(len(bDims) == 0):
+        if(len(bDims) == 0  and  (inpIsLiteral or inpIsScalar)):
+            pass            # Essentially 'numpy.full()'
+        elif(prod(inShape) == prod(outShape)):
+            pass            # essentially this should be a reshape (more test would be fine)
+        elif(len(bDims) == 0  and  (not inpIsLiteral)):
             raise ValueError(f"No broadcast dimension specified.")
         elif(len(bDims) == 1):
             pass    # No test for sorting
@@ -90,19 +111,55 @@ class BroadcastInDimTransformator(JaxIntrinsicTranslatorInterface):
             raise ValueError(f"This function assumes that `broadcast_dimensions` is sorted but it was not (`{bDims}`)")
         #
 
-        # The code is inspired from the `reshape()` function of the python frontend
-        # TODO: Probably poting to view, but this allows to bypass aliasing.
-        inName    = inVarNames[0]
-        outName   = outVarNames[0]
-        inArr     = translator.getArray(inName)
-        outArr    = translator.getArray(outName)
-        inSet     = subsets.Range.from_array(inArr)
-        outSet    = subsets.Range.from_array(outArr)
-        readNode  = eqnState.add_read(inName)
-        writeNode = eqnState.add_write(outName)
-        memlet    = dace.Memlet(data=inName, subset=inSet, other_subset=outSet)
+        if((not (inpIsLiteral or inpIsScalar)) and ((len(bDims) == 1) or (prod(inShape) == prod(outShape)))):
+            # The code is inspired from the `reshape()` function of the python frontend
+            # TODO: Probably poting to view, but this allows to bypass aliasing.
+            inName    = inVarNames[0]
+            outName   = outVarNames[0]
+            inArr     = translator.getArray(inName)
+            outArr    = translator.getArray(outName)
+            inSet     = subsets.Range.from_array(inArr)
+            outSet    = subsets.Range.from_array(outArr)
+            readNode  = eqnState.add_read(inName)
+            writeNode = eqnState.add_write(outName)
+            memlet    = dace.Memlet(data=inName, subset=inSet, other_subset=outSet)
 
-        eqnState.add_nedge(readNode, writeNode, data=memlet)
+            eqnState.add_nedge(readNode, writeNode, data=memlet)
+
+        elif((not (inpIsLiteral or inpIsScalar)) and (len(bDims) != 1)):
+            raise NotImplementedError(f"Multiple broadcast dimensions are not implemented.")
+
+        elif(inpIsLiteral or inpIsScalar):
+            # This code is inspired from the `numpy.full` function.
+            inName    = inVarNames[0]
+            outName   = outVarNames[0]
+            outArr    = translator.getArray(outName)
+
+            if(inpIsScalar):
+                inputs = {'__in_scalar': dace.Memlet(data=inName, subset='0')}
+                tVal   = '__in_scalar'
+            elif(inpIsLiteral):
+                inputs = dict()
+                jaxInVar  = eqn.invars[0]
+                tVal = jaxInVar.val
+                if(isinstance(tVal, np.ndarray)):
+                    tVal = jaxInVar.val.max()
+                #
+            else:
+                raise NotImplementedError("What are you doing man.")
+            #
+
+            eqnState.add_mapped_tasklet(
+                '_fill_',
+                map_ranges={f"__i{dim}": f"0:{s}" for dim, s in enumerate(outShape)},
+                inputs=inputs,
+                code=f"__out = {tVal}",
+                outputs={'__out': dace.Memlet.simple(outName, ",".join([f"__i{dim}" for dim in range(len(outShape))]))},
+                external_edges=True
+            )
+        else:
+            raise NotImplementedError(f"This case is not implemented.")
+        #
 
         return eqnState
     # end def: translateEqn
