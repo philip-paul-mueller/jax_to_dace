a081589ccafa14a65dd9dcb1319e1540e3395131
Christos Kotsalos <kotsaloscv@gmail.com>
2023-11-21 14:59:05 +0100

    Reduction Primitives

--- JaxprToSDFG/translators/reductionTranslator.py
+++ JaxprToSDFG/translators/reductionTranslator.py
@@ -3,10 +3,21 @@
 from JaxprToSDFG import JaxprToSDFG
 from JaxprToSDFG.JaxIntrinsicTranslatorInterface import JaxIntrinsicTranslatorInterface
 
+import ast
+from functools import reduce
+import copy
+from numbers import Integral
+from typing import Any, Callable, List, Tuple, Union
+
 from jax._src.core import JaxprEqn
+
 import dace
 from dace import dtypes, subsets
-from typing import Union
+from dace.memlet import Memlet
+from dace.frontend.python.common import StringLiteral
+from dace.frontend.python import astutils
+from dace.symbolic import pystr_to_symbolic
+from dace.frontend.python.nested_call import NestedCall
 
 
 class ReductionTranslator(JaxIntrinsicTranslatorInterface):
@@ -87,7 +98,6 @@ class ReductionTranslator(JaxIntrinsicTranslatorInterface):
         if not redfunction:
             raise NotImplementedError(f"This reduction primitive [{eqn.primitive.name}] is not implemented")
 
-        from dace.frontend.python.replacements import _reduce, _argminmax
         if identity != None:
             _reduce(None,
                     translator.getSDFG(),
@@ -124,3 +134,222 @@ class ReductionTranslator(JaxIntrinsicTranslatorInterface):
 
 
 # end class(DotGeneralTranslator):
+
+
+# Functions below:
+# copied from Dace (commit 12b998193d966ce656384aaba9dfd32395a4d42c)
+
+def normalize_axes(axes: Tuple[int], max_dim: int) -> List[int]:
+    """ Normalize a list of axes by converting negative dimensions to positive.
+
+        :param dims: the list of dimensions, possibly containing negative ints.
+        :param max_dim: the total amount of dimensions.
+        :return: a list of dimensions containing only positive ints.
+    """
+
+    return [ax if ax >= 0 else max_dim + ax for ax in axes]
+
+def _reduce(pv: Any,
+            sdfg: dace.SDFG,
+            state: dace.SDFGState,
+            redfunction: Callable[[Any, Any], Any],
+            in_array: str,
+            out_array=None,
+            axis=None,
+            identity=None):
+    if out_array is None:
+        inarr = in_array
+        # Convert axes to tuple
+        if axis is not None and not isinstance(axis, (tuple, list)):
+            axis = (axis, )
+        if axis is not None:
+            axis = tuple(pystr_to_symbolic(a) for a in axis)
+            axis = tuple(normalize_axes(axis, len(sdfg.arrays[inarr].shape)))
+
+        input_subset = subsets.Range.from_array(sdfg.arrays[inarr])
+        input_memlet = Memlet.simple(inarr, input_subset)
+        output_shape = None
+
+        # check if we are reducing along all axes
+        if axis is not None and len(axis) == len(input_subset.size()):
+            reduce_all = all(x == y for x, y in zip(axis, range(len(input_subset.size()))))
+        else:
+            reduce_all = False
+
+        if axis is None or reduce_all:
+            output_shape = [1]
+        else:
+            output_subset = copy.deepcopy(input_subset)
+            output_subset.pop(axis)
+            output_shape = output_subset.size()
+        if (len(output_shape) == 1 and output_shape[0] == 1):
+            outarr = sdfg.temp_data_name()
+            outarr, arr = sdfg.add_scalar(outarr, sdfg.arrays[inarr].dtype, sdfg.arrays[inarr].storage, transient=True)
+        else:
+            outarr, arr = sdfg.add_temp_transient(output_shape, sdfg.arrays[inarr].dtype, sdfg.arrays[inarr].storage)
+        output_memlet = Memlet.from_array(outarr, arr)
+    else:
+        inarr = in_array
+        outarr = out_array
+
+        # Convert axes to tuple
+        if axis is not None and not isinstance(axis, (tuple, list)):
+            axis = (axis, )
+        if axis is not None:
+            axis = tuple(pystr_to_symbolic(a) for a in axis)
+            axis = tuple(normalize_axes(axis, len(sdfg.arrays[inarr].shape)))
+
+        # Compute memlets
+        input_subset = subsets.Range.from_array(sdfg.arrays[inarr])
+        input_memlet = Memlet.simple(inarr, input_subset)
+        output_subset = subsets.Range.from_array(sdfg.arrays[outarr])
+        output_memlet = Memlet.simple(outarr, output_subset)
+
+    # Create reduce subgraph
+    inpnode = state.add_read(inarr)
+    rednode = state.add_reduce(redfunction, axis, identity)
+    outnode = state.add_write(outarr)
+    state.add_nedge(inpnode, rednode, input_memlet)
+    state.add_nedge(rednode, outnode, output_memlet)
+
+    if out_array is None:
+        return outarr
+    else:
+        return []
+
+def _argminmax(pv: Any,
+               sdfg: dace.SDFG,
+               state: dace.SDFGState,
+               a: str,
+               axis,
+               func,
+               result_type=dace.int32,
+               return_both=False):
+    nest = NestedCall(pv, sdfg, state)
+
+    assert func in ['min', 'max']
+
+    if axis is None or not isinstance(axis, Integral):
+        raise SyntaxError('Axis must be an int')
+
+    a_arr = sdfg.arrays[a]
+
+    if not 0 <= axis < len(a_arr.shape):
+        raise SyntaxError("Expected 0 <= axis < len({}.shape), got {}".format(a, axis))
+
+    reduced_shape = list(copy.deepcopy(a_arr.shape))
+    reduced_shape.pop(axis)
+
+    val_and_idx = dace.struct('_val_and_idx', idx=result_type, val=a_arr.dtype)
+
+    # HACK: since identity cannot be specified for structs, we have to init the output array
+    reduced_structs, reduced_struct_arr = sdfg.add_temp_transient(reduced_shape, val_and_idx)
+
+    code = "__init = _val_and_idx(val={}, idx=-1)".format(
+        dtypes.min_value(a_arr.dtype) if func == 'max' else dtypes.max_value(a_arr.dtype))
+
+    nest.add_state().add_mapped_tasklet(
+        name="_arg{}_convert_".format(func),
+        map_ranges={'__i%d' % i: '0:%s' % n
+                    for i, n in enumerate(a_arr.shape) if i != axis},
+        inputs={},
+        code=code,
+        outputs={
+            '__init': Memlet.simple(reduced_structs,
+                                    ','.join('__i%d' % i for i in range(len(a_arr.shape)) if i != axis))
+        },
+        external_edges=True)
+
+    nest.add_state().add_mapped_tasklet(
+        name="_arg{}_reduce_".format(func),
+        map_ranges={'__i%d' % i: '0:%s' % n
+                    for i, n in enumerate(a_arr.shape)},
+        inputs={'__in': Memlet.simple(a, ','.join('__i%d' % i for i in range(len(a_arr.shape))))},
+        code="__out = _val_and_idx(idx={}, val=__in)".format("__i%d" % axis),
+        outputs={
+            '__out':
+            Memlet.simple(reduced_structs,
+                          ','.join('__i%d' % i for i in range(len(a_arr.shape)) if i != axis),
+                          wcr_str=("lambda x, y:"
+                                   "_val_and_idx(val={}(x.val, y.val), "
+                                   "idx=(y.idx if x.val {} y.val else x.idx))").format(
+                                       func, '<' if func == 'max' else '>'))
+        },
+        external_edges=True)
+
+    if return_both:
+        outidx, outidxarr = sdfg.add_temp_transient(sdfg.arrays[reduced_structs].shape, result_type)
+        outval, outvalarr = sdfg.add_temp_transient(sdfg.arrays[reduced_structs].shape, a_arr.dtype)
+
+        nest.add_state().add_mapped_tasklet(
+            name="_arg{}_extract_".format(func),
+            map_ranges={'__i%d' % i: '0:%s' % n
+                        for i, n in enumerate(a_arr.shape) if i != axis},
+            inputs={
+                '__in': Memlet.simple(reduced_structs,
+                                      ','.join('__i%d' % i for i in range(len(a_arr.shape)) if i != axis))
+            },
+            code="__out_val = __in.val\n__out_idx = __in.idx",
+            outputs={
+                '__out_val': Memlet.simple(outval, ','.join('__i%d' % i for i in range(len(a_arr.shape)) if i != axis)),
+                '__out_idx': Memlet.simple(outidx, ','.join('__i%d' % i for i in range(len(a_arr.shape)) if i != axis))
+            },
+            external_edges=True)
+
+        return nest, (outval, outidx)
+
+    else:
+        # map to result_type
+        out, outarr = sdfg.add_temp_transient(sdfg.arrays[reduced_structs].shape, result_type)
+        nest(_elementwise)("lambda x: x.idx", reduced_structs, out_array=out)
+        return nest, out
+
+def _elementwise(pv: Any,
+                 sdfg: dace.SDFG,
+                 state: dace.SDFGState,
+                 func: Union[StringLiteral, str],
+                 in_array: str,
+                 out_array=None):
+    """
+    Apply a lambda function to each element in the input.
+    """
+
+    inparr = sdfg.arrays[in_array]
+    restype = sdfg.arrays[in_array].dtype
+
+    if out_array is None:
+        out_array, outarr = sdfg.add_temp_transient(inparr.shape, restype, inparr.storage)
+    else:
+        outarr = sdfg.arrays[out_array]
+
+    func_ast = ast.parse(func.value if isinstance(func, StringLiteral) else func)
+    try:
+        lambda_ast = func_ast.body[0].value
+        if len(lambda_ast.args.args) != 1:
+            raise SyntaxError("Expected lambda with one arg, but {} has {}".format(func, len(lambda_ast.args.arrgs)))
+        arg = lambda_ast.args.args[0].arg
+        replaced_ast = astutils.ASTFindReplace({arg: '__inp'}).visit(lambda_ast.body)
+        body = astutils.unparse(replaced_ast)
+    except AttributeError:
+        raise SyntaxError("Could not parse func {}".format(func))
+
+    code = "__out = {}".format(body)
+
+    num_elements = reduce(lambda x, y: x * y, inparr.shape)
+    if num_elements == 1:
+        inp = state.add_read(in_array)
+        out = state.add_write(out_array)
+        tasklet = state.add_tasklet("_elementwise_", {'__inp'}, {'__out'}, code)
+        state.add_edge(inp, None, tasklet, '__inp', Memlet.from_array(in_array, inparr))
+        state.add_edge(tasklet, '__out', out, None, Memlet.from_array(out_array, outarr))
+    else:
+        state.add_mapped_tasklet(
+            name="_elementwise_",
+            map_ranges={f'__i{dim}': f'0:{N}' for dim, N in enumerate(inparr.shape)},
+            inputs={'__inp': Memlet.simple(in_array, ','.join([f'__i{dim}' for dim in range(len(inparr.shape))]))},
+            code=code,
+            outputs={'__out': Memlet.simple(out_array, ','.join([f'__i{dim}' for dim in range(len(inparr.shape))]))},
+            external_edges=True)
+
+    return out_array
+
